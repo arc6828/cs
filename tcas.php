dac63a4840e594c88f1b021093170a64df5f1f07
arc6828 <chavalit.kow@gmail.com>
2020-09-01 11:25:25 +0700

    TCAS64

--- tcas.php
+++ tcas.php
@@ -2,7 +2,7 @@
 $og_url = "http://cs.vru.ac.th/tcas.php";
 $og_type = "article";
 $og_title = "หลักสูตรวิทยาการคอมพิวเตอร์รับสมัครนักศึกษาปริญญาตรี";
-$og_description = "หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัครนักศึกษาปริญญาตรี TCAS 63 (เรียนจันทร์-ศุกร์ และเรียนเสาร์-อาทิตย์) ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทร. 083-138-0981 ";
+$og_description = "หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัครนักศึกษาปริญญาตรีปีการศึกษา 64 สำหรับเรียนจันทร์-ศุกร์ และปีการศึกษา 63 เทอม 2 สำหรับเรียนเสาร์-อาทิตย์ ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทรด่วน. 083-138-0981 ";
 $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
 ?>
 
@@ -19,7 +19,7 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
     
     <img class="img-thumbnail" src="images/tcas/pr-2020-05-14.jpg" width="100%" > 
     <p class="mt-4" style="font-size : 1.3rem;">
-        หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัครนักศึกษาปริญญาตรี TCAS 63 (เรียนจันทร์-ศุกร์ และเรียนเสาร์-อาทิตย์) ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทร. 083-138-0981 
+        หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัคร <strong>นักศึกษาปริญญาตรีปีการศึกษา 64 สำหรับเรียนจันทร์-ศุกร์ และปีการศึกษา 63 เทอม 2 สำหรับเรียนเสาร์-อาทิตย์</strong> ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทรด่วน. 083-138-0981 
     </p>
 </div>
 <div class="container-fluid bg-light p-5 mt-4" >
@@ -27,10 +27,8 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
         <h3>ช่องทางการสมัคร และระยะเวลาการสมัคร</h3>
         <p style="font-size : 1.2rem;">ผ่านเว็บไซต์กลางของมหาวิทยาลัย : <a href="http://ent.vru.ac.th/">http://ent.vru.ac.th/</a></p>
         <ul style="list-style-type:none;">
-        <li>ปริญญาตรีเรียนจันทร์ - ศุกร์ สมัครเรียนได้ตั้งแต่วันนี้ - 14 มิถุนายน 2563 <span class="text-danger">(ปิดรับสมัคร)</span></li>
-        <li>ปริญญาตรีเรียนเสาร์ - อาทิตย์ สมัครเรียนได้ตั้งแต่วันนี้ - 12 มิถุนายน 2563  <span class="text-danger">(ปิดรับสมัคร)</span></li>
-        
-        <li>ปริญญาตรีเรียนเสาร์ - อาทิตย์ สมัครเรียนได้ตั้งแต่วันนี้เป็นต้นไป  <span class="text-success">(เปิดเทอม พฤศจิกายน 2563)</span></li>
+            <li>ปริญญาตรีเรียนจันทร์ - ศุกร์ สมัครเรียนได้ตั้งแต่ <strong>วันนี้ - มิถุนายน 2564 </strong> <span class="text-danger"></span></li>
+            <li>ปริญญาตรีเรียนเสาร์ - อาทิตย์ สมัครเรียนได้ตั้งแต่ <strong>วันนี้ - 15 ตุลาคม 2563</strong>  <span class="text-success">(เปิดเทอม พฤศจิกายน 2563)</span></li>
         </ul>
         <div class="text-center">
             <div class="py-2">
@@ -48,33 +46,42 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
 </div>
 
 <div class="container" >
-
+    <!-- START NORMAL -->
     <div class="mt-4">
-        <h3 id="schedule">ตารางการรับสมัครนักศึกษา TCAS 63 (ปริญญาตรี จันทร์-ศุกร์)</h3>
+        <h3 class="text-primary">ตารางการรับสมัครนักศึกษา TCAS 64 (ปริญญาตรี จันทร์-ศุกร์)</h3>
+        <?php include('tcas-detail.php') ?>
         <div class="text-center">
-            <img class="img-thumbnail"  src="images/tcas/tcas63-schedule.png" width="100%">
+            <img class="img-thumbnail"  src="images/tcas/tcas64-schedule-normal.png" width="100%">
             <p>อ้างอิง <a href="http://ent.vru.ac.th/UPDATE/PDFfile/new_tcas63.pdf">http://ent.vru.ac.th/UPDATE/PDFfile/new_tcas63.pdf</a></p>
         </div>
     </div>
+    <!-- START NORMAL -->
+
+    <!-- START SPECIAL -->
     <div class="mt-4">
-        <h3 id="schedule">ตารางการรับสมัครนักศึกษาปริญญาตรี เสาร์-อาทิตย์ 63 เทอม 2 </h3>
-        
-        <div class="text-center d-none">
-            <img class="img-thumbnail"  src="images/tcas/tcas63-schedule2.png" width="100%" >
-            <p>อ้างอิง <a href="http://ent.vru.ac.th/Bachelor_special_Register/PDFfile/new63/new_bch_27052563.pdf">http://acad.vru.ac.th/about_acad/staff/news/2/news425.pdf</a></p>
+        <h3 class="text-primary">ตารางการรับสมัครนักศึกษาปริญญาตรี เสาร์-อาทิตย์ 63 เทอม 2 </h3>
+        <ul>            
+            <li><strong>เปิดรับสมัครและชำระเงินค่าสมัครตั้งแต่ 1 กันยายน  ถึง 15 ตุลาคม 2563</strong></li>
+            <li>ผู้สมัครแนบวุฒิการศึกษาในระบบรับสมัครตั้งแต่ 1 กันยายน  ถึง 15 ตุลาคม 2563</li>
+            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษาและสาขาวิชาที่เปิดเรียนได้วันที่ 17 ตุลาคม 2563</li>
+            <li>ผู้สมัครเลือกสาขาวิชาใหม่ในสาขาวิชาที่ไม่สามารถเปิดเรียนได้ ในระบบรับสมัคร วันที่ 19 - 22 ตุลาคม 2563</li>
+            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษา (เพิ่มเติม)วันที่ 24 ตุลาคม 2563</li>
+            <li> ชำระเงินลงทะเบียนที่เคาน์เตอร์เซอร์วิส 7-11/มหาวิทยาลัยวันที่ 25 - 31 ตุลาคม 2563</li>
+            <li>ปฐมนิเทศวันที่ 1 พฤศจิกายน  2563</li>
+            <li>เปิดภาคการศึกษาที่ 2/2563วันที่ 21 พฤศจิกายน  2563</li>          
+        </ul>        
+        <div class="text-center">
+            <img class="img-thumbnail"  src="images/tcas/tcas64-schedule-special.png" width="100%" style="max-width:800px;" >
+            <p>อ้างอิง <a href="http://acad.vru.ac.th/about_acad/staff/news/2/news436.pdf">http://acad.vru.ac.th/about_acad/staff/news/2/news436.pdf</a></p>
     
         </div>
-        <div class="text-center h4 text-danger p-4">
-            รอประกาศอย่างเป็นทางการจากฝ่ายทะเบียน
-    
-        </div>
-        
     </div>
+    <!-- START SPECIAL -->
 
 </div>
 <div class="container-fluid bg-light p-5 mt-4" >
     <div class="container text-center" >
-        <h3>สมัครเรียนที่นี่ ปริญญาตรี ภาคจันทร์-ศุกร์ และภาคเสาร์-อาทิตย์</h3>
+        <h3 >สมัครเรียนที่นี่ ปริญญาตรี ภาคจันทร์-ศุกร์ และภาคเสาร์-อาทิตย์</h3>
         <div class="text-center">
             <div class="py-2">
                 <a class="btn btn-primary" href="http://ent.vru.ac.th">กรอกใบสมัครเรียน</a>
