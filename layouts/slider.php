e45275f3afce694a8f3f8a669277f852e5f7354c
arc6828 <chavalit.kow@gmail.com>
2020-10-13 13:46:50 +0700

    Technology Stack

--- layouts/slider.php
+++ layouts/slider.php
@@ -1,22 +1,12 @@
 <div class="site-block-wrap">
-    <div class="owl-carousel with-dots">
-        <!--div style="background-image: url(images/slide/0.jpg);" class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section">
-            <div class="container">
-                <div class="row align-items-center justify-content-center">
-                <div class="col-md-6 mt-lg-5 text-center">
-                    <h1 class="text-shadow d-none">Buy &amp; Sell Property Here</h1>
-                    <p class="mb-5 text-shadow d-none">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident vitae, aut inventore repellendus. Iusto, assumenda! </p>
-                    <p><a href="#" target="_blank" class="btn btn-primary px-5 py-3 d-none">Get Started</a></p>
-                </div>
-                </div>
-            </div>
-        </div-->  
+    <div class="owl-carousel with-dots">        
         <div style="background-image : url(images/slide/1.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
         <div style="background-image : url(images/slide/2.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
         <div style="background-image : url(images/slide/3.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
         <div style="background-image : url(images/slide/4.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
         <div style="background-image : url(images/slide/5.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
         <div style="background-image : url(images/slide/8.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
-        <div style="background-image : url(images/slide/9.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
+        <div style="background-image : url(images/slide/9.png);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
+        <div style="background-image : url(images/slide/10.jpg);"  class="site-blocks-cover overlay overlay-2" data-aos="fade" id="home-section"></div>
     </div>   
 </div> 
