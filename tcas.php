2b6bbd1ea730ea230182c2d32a452fe56ee9649b
arc6828 <chavalit.kow@gmail.com>
2020-10-13 15:23:11 +0700

    Teachnology stack

--- tcas.php
+++ tcas.php
@@ -140,8 +140,23 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
     </div>
 </div>
 
-<div class="container text-center mt-4" >
-    <img class="img-thumbnail"  src="images/tcas/pr-2020-04-27.jpg" style="max-width:500px; width:100%;">
+<div class="container pt-5" >
+    <img class="img-thumbnail d-none"  src="images/tcas/pr-2020-04-27.jpg" style="max-width:500px; width:100%;">
+    <h2>Technology Stack 2021</h2>
+    <div class="mt-4 text-center ">
+        <img class="img-thumbnail"  src="images/technology-stack.jpg" style="width:100%;">
+    </div>
+    <div class="mt-4" >
+        <h5>แต่ละชั้นปีเรียนอะไร ...</h5>
+        <ul>
+            <li>ปี 1 ฺคุณจะได้เรียนรู้การเขียนโปรแกรมพื้นฐาน เช่น C Python รวมไปถึงการพัฒนา Web Frontend เช่น HTML CSS JS รวมถึงหัดใช้งาน git service</li>
+            <li>ปี 2 คุณจะเริ่มได้เริ่มเรียนรู้อะไรเกี่ยวกับ Backend ต่างๆ ภาษา Object Oriented เช่น ตระกูล java รวมไปถึงฐานข้อมูลต่างๆ ที่อยู่ในความนิยม</li>
+            <li>ปี 3 คุณจะได้ลองทำระบบที่มีการนำ Backend และ Frondend มารวมกัน ทั้งในรูปแบบ Web Application , Mobile Application, Internet Of Thing หรือแม้กระทั่ง Application Standalone</li>
+            <li>ปี 4 คุณจะได้ทำความรู้จักกับระบบ Infrastucture on cloud ต่างๆ ซึ่งมีอยู่ด้วยกันหลายค่าย รวมไปถึงความรู้พื้นฐานเกี่ยวกับ data science และปัญญาประดิษฐ์ </li>
+            <li>สุดท้าย รับประกันได้ว่าเมื่อคุณได้เก็บเกี่ยวทุกอย่างที่อยู่ในหลักสูตรเราแล้ว คุณพร้อมแล้วสำหรับการเริ่มต้นเป็นโปรแกรมเมอร์ได้เลย</li>
+        </ul>
+    </div>
+
     
 </div>
 
