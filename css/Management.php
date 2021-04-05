26541e4a293fdc628d09055d83d2395c39b544f7
Denthai2011 <ynloveys@gmail.com>
2021-04-05 12:44:00 +0700

    Create Management.php

---
+++ css/Management.php
@@ -0,0 +1,8 @@
+<?php include("layouts/header.php"); ?>
+<div class="container">
+   <br />
+   <h2>&starf;&nbsp;โครงสร้างบริหารหลักสูตร</h2>
+   <hr>
+	   <a><img class="img-responsive" src="images/Structure-Course-CS.jpg"></a>	   
+</div>
+<?php include("layouts/footer.php"); ?>
