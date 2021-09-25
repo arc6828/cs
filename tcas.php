b448d543aed699ce47f55fcc3befbb31dea30022
Nawathpolkaw <nawathpolkaw@gmail.com>
2021-09-25 21:21:07 +0700

    add

--- tcas.php
+++ tcas.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $og_url = "http://cs.vru.ac.th/tcas.php";
 $og_type = "article";
 $og_title = "หลักสูตรวิทยาการคอมพิวเตอร์รับสมัครนักศึกษาปริญญาตรี";
@@ -9,24 +9,25 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
 <?php include("layouts/header.php") ?>
 <!-- start content -->
 
-<style>        
-        
+<style>
+
 </style>
 <!-- Introduction -->
-<div class="container mt-4" >
-    <h1 ><i class="fa fa-star"></i> รับสมัครนักศึกษาปริญญาตรี (จันทร์-ศุกร์ และ เสาร์-อาทิตย์)</h1>
+<div class="container mt-4">
+    <h1><i class="fa fa-star"></i> รับสมัครนักศึกษาปริญญาตรี (จันทร์-ศุกร์ และ เสาร์-อาทิตย์)</h1>
     <hr>
-    
-    <img class="img-thumbnail" src="images/tcas/pr-2020-05-14.jpg" width="100%" > 
+
+    <img class="img-thumbnail" src="images/tcas/pr-2020-05-14.jpg" width="100%">
     <p class="mt-4" style="font-size : 1.3rem;">
-        หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัคร <strong>นักศึกษาปริญญาตรีปีการศึกษา 64 สำหรับเรียนจันทร์-ศุกร์ และเรียนเสาร์-อาทิตย์</strong> ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทรด่วน. 083-138-0981 
+        หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ รับสมัคร <strong>นักศึกษาปริญญาตรีปีการศึกษา 64 สำหรับเรียนจันทร์-ศุกร์ และเรียนเสาร์-อาทิตย์</strong> ตั้งแต่วันนี้เป็นต้นไป สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทรด่วน. 083-138-0981
     </p>
 </div>
 
 <!-- ช่องทางการสมัคร -->
-<div class="container-fluid bg-light py-5 mt-4" >
-    <div class="container text-center" >
-        <h3>ช่องทางการสมัคร และระยะเวลาการสมัคร</h3>
+<div class="container-fluid bg-light py-5 mt-4">
+    <div class="container text-center">
+        <img class="img-thumbnail" src="images/รับสมัครปี64.png" width="100%">
+        <h3 class="mt-4">ช่องทางการสมัคร และระยะเวลาการสมัคร</h3>
         <p style="font-size : 1.2rem;">ผ่านเว็บไซต์กลางของมหาวิทยาลัย : <a href="http://ent.vru.ac.th/">http://ent.vru.ac.th/</a></p>
         <ul style="list-style-type:none;">
             <li>ปริญญาตรีเรียนจันทร์ - ศุกร์ สมัครเรียนได้ตั้งแต่ <strong>วันนี้ - พฤษภาคม 2564 </strong> <span class="text-danger"></span></li>
@@ -35,15 +36,15 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
         </ul>
         <div class="text-center">
             <div class="py-2">
-                <a class="btn btn-primary  mb-4" href="http://ent.vru.ac.th"  target="_blank">
+                <a class="btn btn-primary  mb-4" href="http://ent.vru.ac.th" target="_blank">
                     สมัครเรียน<br>ผ่านส่วนกลาง<br>ทุกประเภท<br>(ค่าสมัคร 300 บาท)
-                </a> 
-             
-                <a class="btn btn-outline-primary  mb-4" href="https://forms.gle/4WYGkwpKAhX387Du6"  target="_blank">
+                </a>
+
+                <a class="btn btn-outline-primary  mb-4" href="https://forms.gle/4WYGkwpKAhX387Du6" target="_blank">
                     สมัครเรียน<br>ผ่านคณะ<br> ประเภทจันทร์ - ศุกร์ <br>(ฟรีค่าสมัคร)
                 </a>
-             
-                <a class="btn btn-outline-secondary  mb-4" href="https://forms.gle/BxYpmgHTJ98gytQR9"  target="_blank">
+
+                <a class="btn btn-outline-secondary  mb-4" href="https://forms.gle/BxYpmgHTJ98gytQR9" target="_blank">
                     ลิงค์สำรองกรอก<br>ผ่าน Google Form <br> ทุกประเภท <br>(ค่าสมัคร 300 บาท)
                 </a>
             </div>
@@ -57,36 +58,37 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
 </div>
 
 <!-- กำหนดการ -->
-<div class="container" >
+<div class="container">
     <!-- START NORMAL -->
     <div class="mt-4">
-        <h3 class=""><i class="fa fa-calendar"></i>  ตารางการรับสมัครนักศึกษา TCAS 64 (ปริญญาตรี จันทร์-ศุกร์)</h3>
+        <h3 class=""><i class="fa fa-calendar"></i> ตารางการรับสมัครนักศึกษา TCAS 64 (ปริญญาตรี จันทร์-ศุกร์)</h3>
         <?php include('tcas-detail.php') ?>
         <div class="text-center">
-            <img class="img-thumbnail"  src="images/tcas/tcas64-schedule-normal.png" width="100%">
-            <p>อ้างอิง <a href="http://acad.vru.ac.th/about_acad/staff/news/2/news437.pdf">PDF ประกาศจันทร์ - ศุกร์ </a></p>
+            <img class="img-thumbnail" src="images/tcas/tcas64-schedule-normal.png" width="100%">
+            <p>อ้างอิง <a href="http://acad.vru.ac.th/about_acad/staff/fileup_regulations/122.pdf">PDF ประกาศจันทร์ - ศุกร์ </a></p>
         </div>
     </div>
     <!-- END NORMAL -->
 
     <!-- START SPECIAL -->
     <div class="mt-4">
-        <h3 class=""><i class="fa fa-calendar"></i>  ตารางการรับสมัครนักศึกษาปริญญาตรี เสาร์-อาทิตย์ 64 </h3>
-        <div><a href="http://acad.vru.ac.th/about_acad/staff/news/2/news454.pdf" target="_blank">PDF ประกาศรับสมัครภาคเสาร์ - อาทิตย์</a>  </div>
-        <ul>            
-            
-            <li><strong>เปิดรับสมัครออนไลน์  แนบวุฒิการศึกษา และชำระเงินค่าสมัครตั้งแต่วันนี้  ถึง 16 พฤษภาคม 2564</strong></li>
-            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษาและสาขาวิชาที่เปิดเรียนได้วันที่ 20 พฤษภาคม 2564</li>
-            <li>ผู้สมัครเลือกสาขาวิชาใหม่ในสาขาวิชาที่ไม่สามารถเปิดเรียนได้ ในระบบรับสมัคร วันที่ 21 - 26 พฤษภาคม 2564</li>
-            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษา (เพิ่มเติม)วันที่ 30 พฤษภาคม 2564</li>
-            <li>ชำระเงินลงทะเบียนที่เคาน์เตอร์เซอร์วิส 7-11/มหาวิทยาลัยวันที่ 1 - 15 มิถุนายน 2564</li>            
-            <li>ปฐมนิเทศภายในมิถุนายน 2564 (รอประกาศอย่างเป็นทางการ)</li>
-            <li>เปิดภาคการศึกษาที่ 1/2564 วันที่ 26 มิถุนายน 2564</li>          
-        </ul>        
+        <h3 class=""><i class="fa fa-calendar"></i> ตารางการรับสมัครนักศึกษาปริญญาตรี เสาร์-อาทิตย์ ปีการศึกษา 2/2564 </h3>
+        <div><a href="http://acad.vru.ac.th/about_acad/staff/news/2/news454.pdf" target="_blank">PDF ประกาศรับสมัครภาคเสาร์ - อาทิตย์</a> </div>
+        <ul>
+
+            <li><strong>เปิดรับสมัครและชำระเงินค่าสมัคร วันที่ 1 กันยายน 2564 – วันที่ 15 ตุลาคม 2564</strong></li>
+            <li>ผู้สมัครแนบวุฒิการศึกษาในระบบรับสมัคร วันที่ 1 กันยายน 2564 – วันที่ 15 ตุลาคม 2564</li>
+            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษาและสาขาวิชาที่เปิดเรียนได้ ในระบบรับสมัคร วันที่ 17 ตุลาคม 2564</li>
+            <li>ผู้มีสิทธิ์เข้าศึกษาชำระเงินค่าธรรมเนียมแรกเข้า ที่เคาน์เตอร์เซอร์วิส 7-11/มหาวิทยาลัย วันที่ 18 - 29 ตุลาคม 2564</li>
+            <li>ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษา (เพิ่มเติม) ในระบบรับสมัคร วันที่ 30 ตุลาคม 2564</li>
+            <li>ชำระเงินลงทะเบียนที่เคาน์เตอร์เซอร์วิส 7-11/มหาวิทยาลัย วันที่ 1 – 12 พฤศจิกายน 2564</li>
+            <li>ปฐมนิเทศ วันที่ 14 พฤศจิกายน 2564</li>
+            <li>เปิดภาคการศึกษาที่ 2/2564 วันที่ 20 พฤศจิกายน 2564</li>
+        </ul>
         <div class="text-center">
-            <img class="img-thumbnail"  src="images/tcas/tcas64-schedule-special.png" width="100%" style="max-width:800px;" >
+            <img class="img-thumbnail" src="images/table2_64.JPG" width="100%" style="max-width:800px;">
             <p>อ้างอิง <a href="http://acad.vru.ac.th/about_acad/staff/fileup_Schedule/2564_20201117-1.pdf" target="_blank">PDF ประกาศ เสาร์ - อาทิตย์</a></p>
-    
+
         </div>
     </div>
     <!-- END SPECIAL -->
@@ -94,16 +96,16 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
 </div>
 
 <!-- อัตราค่าธรรมเนียมการศึกษาวิทยาการคอมพิวเตอร์ -->
-<div class="container-fluid bg-light py-5"   >
-    <div class="container "  >
-        <h3 class="container  anchor" id="fee" >
-            <i class="fa fa-percent"></i>  อัตราค่าธรรมเนียมการศึกษาวิทยาการคอมพิวเตอร์ <a href="#fee">#</a>
+<div class="container-fluid bg-light py-5">
+    <div class="container ">
+        <h3 class="container  anchor" id="fee">
+            <i class="fa fa-percent"></i> อัตราค่าธรรมเนียมการศึกษาวิทยาการคอมพิวเตอร์ <a href="#fee">#</a>
         </h3>
         <div class="text-center">
             <div class="py-2">
                 <div class="table-responsive">
                     <table class="table table-striped ">
-                        <thead>                    
+                        <thead>
                             <tr>
                                 <th>ภาคการเรียน</th>
                                 <th>ช่วงเวลา</th>
@@ -111,27 +113,27 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
                                 <th>เสาร์ - อาทิตย์</th>
                             </tr>
                         </thead>
-                        <tbody>                      
+                        <tbody>
                             <tr>
                                 <td>ภาคเรียนที่ 1</td>
                                 <td>กรกฏาคม - ตุลาคม</td>
                                 <td>12,000 บาท</td>
                                 <td>12,000 บาท</td>
-                            </tr> 
+                            </tr>
                             <tr>
                                 <td>ภาคเรียนที่ 2</td>
                                 <td>พฤศจิกายน - กุมภาพันธ์</td>
                                 <td>12,000 บาท</td>
                                 <td>12,000 บาท</td>
-                            </tr>                         
+                            </tr>
                             <tr>
                                 <td>ภาคเรียนที่ 3</td>
                                 <td>มีนาคม - พฤษภาคม</td>
                                 <td>-</td>
                                 <td>ไม่เกิน 6,000 บาท*</td>
-                            </tr>   
+                            </tr>
                         </tbody>
-                        <tfoot>                     
+                        <tfoot>
                             <tr>
                                 <th>รวม</th>
                                 <th>ตลอดปี</th>
@@ -145,32 +147,32 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
             </div>
             <div class="text-center">
                 <div class="py-2">
-                    <a class="btn btn-primary mb-4" href="http://ent.vru.ac.th"  target="_blank">
+                    <a class="btn btn-primary mb-4" href="http://ent.vru.ac.th" target="_blank">
                         สมัครเรียน<br>ผ่านส่วนกลาง<br>ทุกประเภท<br>(ค่าสมัคร 300 บาท)
-                    </a> 
-                
-                    <a class="btn btn-outline-primary mb-4" href="https://forms.gle/4WYGkwpKAhX387Du6"  target="_blank">
+                    </a>
+
+                    <a class="btn btn-outline-primary mb-4" href="https://forms.gle/4WYGkwpKAhX387Du6" target="_blank">
                         สมัครเรียน<br>ผ่านคณะ<br> ประเภทจันทร์ - ศุกร์ <br>(ฟรีค่าสมัคร)
                     </a>
-                
-                    <a class="btn btn-outline-secondary mb-4" href="https://forms.gle/BxYpmgHTJ98gytQR9"  target="_blank">
+
+                    <a class="btn btn-outline-secondary mb-4" href="https://forms.gle/BxYpmgHTJ98gytQR9" target="_blank">
                         ลิงค์สำรองกรอก<br>ผ่าน Google Form <br> ทุกประเภท <br>(ค่าสมัคร 300 บาท)
                     </a>
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div>
 
 <!-- Technology Stack 2021 -->
-<div class="container pt-5" >
-    <img class="img-thumbnail d-none"  src="images/tcas/pr-2020-04-27.jpg" style="max-width:500px; width:100%;">
-    <h2><i class="fa fa-rocket"></i>  Technology Stack 2021</h2>
+<div class="container pt-5">
+    <img class="img-thumbnail d-none" src="images/tcas/pr-2020-04-27.jpg" style="max-width:500px; width:100%;">
+    <h2><i class="fa fa-rocket"></i> Technology Stack 2021</h2>
     <div class="mt-4 text-center ">
-        <img class="img-thumbnail"  src="images/technology-stack.jpg" style="width:100%;">
+        <img class="img-thumbnail" src="images/technology-stack.jpg" style="width:100%;">
     </div>
-    <div class="mt-4" >
+    <div class="mt-4">
         <h5>แต่ละชั้นปีเรียนอะไร ...</h5>
         <ul>
             <li>ปี 1 ฺคุณจะได้เรียนรู้การเขียนโปรแกรมพื้นฐาน เช่น C Python รวมไปถึงการพัฒนา Web Frontend เช่น HTML CSS JS รวมถึงหัดใช้งาน git service</li>
@@ -181,27 +183,27 @@ $og_image = "http://cs.vru.ac.th/images/tcas/pr-2020-05-14.jpg";
         </ul>
     </div>
 
-    
+
 </div>
 
 <!-- ติดตามข่าวสารได้ที่นี่ -->
-<div class="container pt-5" >
+<div class="container pt-5">
     <h2><i class="fa fa-bullhorn"></i> ติดตามข่าวสารการสมัครได้ที่นี่</h2>
     <div class="">
         <p>หลักจากที่คุณได้สมัครเข้ามาแล้ว คุณสามารถติดตามข่าวสารการสมัครของคุณ รวมถึงการประกาศต่างๆ จากทางมหาวิทยาลัยอย่างใกล้ชิดให้ที่ Line กลุ่มของหลักสูตร โดยจะแบ่งเป็น 2 กลุ่ม ได้แก่่ </p>
         <div class="row text-center">
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line-Mon-Fri.jpg" style="max-width:300px; width:100%;">
+                <img class="img-thumbnail" src="images/tcas/Line-Mon-Fri.jpg" style="max-width:300px; width:100%;">
                 <div><strong>CS VUR #64 (จันทร์ - ศุกร์)</strong></div>
                 <div><a href="http://line.me/ti/g/MbQa_Tw6Bo">http://line.me/ti/g/MbQa_Tw6Bo</a></div>
             </div>
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line-Sat-Sun.jpg" style="max-width:300px; width:100%;">
+                <img class="img-thumbnail" src="images/tcas/Line-Sat-Sun.jpg" style="max-width:300px; width:100%;">
                 <div><strong>CS VUR #64 (เสาร์ - อาทิตย์)</strong></div>
                 <div><a href="http://line.me/ti/g/NypQRk0nzk">http://line.me/ti/g/NypQRk0nzk</a></div>
             </div>
         </div>
     </div>
 </div>
-<!-- end content --> 
-<?php include("layouts/footer.php") ?>  
+<!-- end content -->
+<?php include("layouts/footer.php") ?>
