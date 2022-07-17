bef9b3d65d1fd53c0e7e35ada8c2da822a6ecd2d
Chavalit Koweerawong <chavalit.kow@gmail.com>
2022-07-17 22:45:56 +0700

    Update footer.php by adding google analytics

--- layouts/footer.php
+++ layouts/footer.php
@@ -171,6 +171,24 @@
         });*/
     });
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXZ2L917CN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NXZ2L917CN');
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXZ2L917CN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NXZ2L917CN');
+</script>
 
     
   </body>
