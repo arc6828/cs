16d4c6632ef70e60650d557f3cc27b591d2f45d5
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-12-06 11:36:53 +0700

    Update content-detail.php

--- content/content-detail.php
+++ content/content-detail.php
@@ -46,49 +46,86 @@ $tagged = $article_set->tagged;
             <div class="col-md-8">
                 <div>
                     <?php foreach (json_decode($article->category) as $c) { ?>
-                    <span class="badge rounded-pill text-bg-light"><?= $c ?></span>
+                        <span class="badge rounded-pill text-bg-light"><?= $c ?></span>
                     <?php } ?>
                 </div>
                 <div>
                     <?= html_entity_decode($article->contentEncoded) ?>
                 </div>
-                {{-- <div> {{ html_entity_decode($article->contentEncoded) }}
-            </div> --}}
 
-            <div>
-                @foreach (json_decode($article->category) as $c)
-                <span class="badge rounded-pill text-bg-light">{{ $c }}</span>
-                @endforeach
+
+            </div>
+            <div class="col-md-4">
+                <div class="px-4 border-start">
+                    <h2 class="h4">บทความล่าสุด</h2>
+
+
+                    <div class="row g-4">
+                        <?php foreach ($latest as $item) { ?>
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <img src="<?= $item->image_url ?>" class="card-img-top" alt="<?= $item->title ?>">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?= $item->title ?></h5>
+                                        <p class="card-text"><?= $item->creator ?></p>
+                                        <p class="card-text"><?= $item->pubDate ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+
+
+
+
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="px-4 border-start">
-                <h2 class="h4">บทความล่าสุด</h2>
 
-                
-                <div class="row g-4">
-                    <?php foreach ($latest as $item) {?>
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <img src="<?= $item->image_url ?>" class="card-img-top" alt="<?= $item->title ?>">
+
+    </div>
+</section>
+<br />
+<section>
+    <div class="container">
+        <h2 class="h4">อาจารย์ผู้เขียนบทความ</h2>
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-xl-1 gutter-2">
+
+            <div class="col p-1 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                <div class="card h-100">
+                    <div class="row no-gutters">
+                        <div class="col-xl-2 col-lg-2 col-md-3">
+                            <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_105114_edit.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="col-xl-10 col-lg-10 col-md-9">
                             <div class="card-body">
-                                <h5 class="card-title"><?= $item->title ?></h5>
-                                <p class="card-text"><?= $item->creator ?></p>
-                                <p class="card-text"><?= $item->pubDate ?></p>
+                                <h5 style="font-size: 1.2rem;">วิวัฒน์ ชินนาทศิริกุล</h5>
+                                <div class="text-black-50 prompt">ผู้ช่วยศาสตราจารย์</div>
+                                <div>
+                                    <div>
+                                        <sup>1</sup>
+                                        พบ.ม. สถิติประยุกต์ (ระบบและการจัดการสารสนเทศ) - สถาบันบัณฑิตพัฒนบริหารศาสตร์ (NIDA)
+                                    </div>
+                                    <div>
+                                        <sup>2</sup>
+                                        กศ.บ. (คณิตศาสตร์) - มหาวิทยาลัยศรีนครินทรวิโรฒประสานมิตร
+                                    </div>
+                                </div>
+                                <!-- <div>  </div> -->
+                                <div class="text-black-50"><i class="fa fa-map-marker"></i> IT201 อาคาร 75 ปีฯ</div>
+                                <div class="text-black-50"> <label>wiwat@vru.ac.th </label></div>
                             </div>
                         </div>
                     </div>
-                    <?php } ?>
+
+
+
                 </div>
-
-
-
-
             </div>
+            <!-- End Team Member-->
+
+
         </div>
-    </div>
-
-
     </div>
 </section>
 <br />
@@ -98,24 +135,24 @@ $tagged = $article_set->tagged;
             <div class="col-md-12">
                 <h2 class="h4">บทความอื่นๆ</h2>
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3  row-cols-xl-4 g-4">
-                    
+
                     <?php foreach ($tagged as $item) { ?>
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src="<?= $item->image_url ?>" class="card-img-top" alt="<?= $item->title ?>">
-                            <div class="card-body">
-                                <h6 class="card-title"><?= mb_substr($item->title, 0, 50) ?></h6>
-                                <p class="card-text"><?= $item->creator ?></p>
-                                <p class="card-text"><?= $item->pubDate ?></p>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="<?= $item->image_url ?>" class="card-img-top" alt="<?= $item->title ?>">
+                                <div class="card-body">
+                                    <h6 class="card-title"><?= mb_substr($item->title, 0, 50) ?></h6>
+                                    <p class="card-text"><?= $item->creator ?></p>
+                                    <p class="card-text"><?= $item->pubDate ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php } ?>
                 </div>
             </div>
         </div>
     </div>
 </section>
-</section>
+
 
 <?php include("../layouts/footer2.php"); ?>
