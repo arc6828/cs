75541652747a54deb095ad29a0aa921620a2ac9e
arc6828 <chavalit.kow@gmail.com>
2024-09-14 17:04:35 +0700

    Line

--- layouts/group-line-qr.php
+++ layouts/group-line-qr.php
@@ -4,14 +4,14 @@
         <p>หลักจากที่คุณได้สมัครเข้ามาแล้ว คุณสามารถติดตามข่าวสารการสมัครของคุณ รวมถึงการประกาศต่างๆ จากทางมหาวิทยาลัยอย่างใกล้ชิดให้ที่ Line กลุ่มของหลักสูตร โดยจะแบ่งเป็น 2 กลุ่ม ได้แก่่ </p>
         <div class="row text-center">
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line67-Mon-Fri.jpg" style="max-width:300px; width:100%;">
+                <img class="img-thumbnail"  src="images/tcas/Line68-Mon-Fri.jpg" style="max-width:300px; width:100%;">
                 <div><strong>CS-VRU#68 (จันทร์-ศุกร์)</strong></div>
-                <div><a href="https://line.me/R/ti/g/fB3MaWFZSV">https://line.me/R/ti/g/fB3MaWFZSV</a></div>
+                <div><a href="https://line.me/R/ti/g/3KvnAdal0D">https://line.me/R/ti/g/3KvnAdal0D</a></div>
             </div>
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line67-Sat-Sun.jpg" style="max-width:300px; width:100%;">
+                <img class="img-thumbnail"  src="images/tcas/Line68-Sat-Sun.jpg" style="max-width:300px; width:100%;">
                 <div><strong>CS-VRU#68 (เสาร์-อาทิตย์)</strong></div>
-                <div><a href="https://line.me/R/ti/g/jZw4PXTk9q">https://line.me/R/ti/g/jZw4PXTk9q</a></div>
+                <div><a href="https://line.me/R/ti/g/SciDXAHVfJ">https://line.me/R/ti/g/SciDXAHVfJ</a></div>
             </div>
         </div>
     </div>
