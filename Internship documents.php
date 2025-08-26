067e067419ddefa8378b5406ede8b05ff6f55099
arc6828 <chavalit.kow@gmail.com>
2025-08-26 14:54:48 +0700

    Intership

--- Internship documents.php
+++ Internship documents.php
@@ -21,6 +21,9 @@
 <a href="uploads/Internship/VRU_Co14.pdf"><font color="black">&#187;แบบประเมินรายงานนักศึกษาสหกิจศึกษา</a> <br />
 <a href="uploads/Internship/VRU_Co15.pdf"><font color="black">&#187;แบบแจ้งยืนยันส่งรายงานการปฏิบัติงาน</a> <br />
 <a href="uploads/Internship/VRU_Co16.pdf"><font color="black">&#187;แบบแจ้งรายละเอียดเกี่ยวกับการปฏิบัติงานสหกิจศึกษา</a> <br />
+<a href="uploads/Internship/VRU_Co17.docx"><font color="black">&#187;แบบส่งคำร้องสำหรับขอจดหมายขอความอนุเคราะห์สถานปฏิบัติงานเพื่อฝึกประสบการณ์วิชาชีพสหกิจศึกษา  คณะวิทยาศาสตร์และเทคโนโลยี</a> <br />
+<a href="uploads/Internship/Form-06.pdf"><font color="black">&#187;แบบประเมินการฝึกงานของนักศึกษา(สำหรับอาจารย์นิเทศ)</a> <br />
+<a href="uploads/Internship/Form-07.pdf"><font color="black">&#187;แบบประเมินผลการปฏิบัติงานนักศึกษาฝึกงาน(สำหรับสถานประกอบการ)</a> <br />
 
 </div>  
 <?php include("layouts/footer.php"); ?>
