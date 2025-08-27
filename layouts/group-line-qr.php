10a9fe970638b7fa37d6379963660f16af300222
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-08-27 14:23:30 +0700

    QR Code

--- layouts/group-line-qr.php
+++ layouts/group-line-qr.php
@@ -4,14 +4,14 @@
         <p>หลักจากที่คุณได้สมัครเข้ามาแล้ว คุณสามารถติดตามข่าวสารการสมัครของคุณ รวมถึงการประกาศต่างๆ จากทางมหาวิทยาลัยอย่างใกล้ชิดให้ที่ Line กลุ่มของหลักสูตร โดยจะแบ่งเป็น 2 กลุ่ม ได้แก่่ </p>
         <div class="row text-center">
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line68-Mon-Fri.jpg" style="max-width:300px; width:100%;">
-                <div><strong>CS-VRU#68 (จันทร์-ศุกร์)</strong></div>
-                <div><a href="https://line.me/R/ti/g/3KvnAdal0D">https://line.me/R/ti/g/3KvnAdal0D</a></div>
+                <img class="img-thumbnail"  src="images/tcas/Line69-Mon-Fri.jpg" style="max-width:300px; width:100%;">
+                <div><strong>CS-VRU#69 (จันทร์-ศุกร์)</strong></div>
+                <div><a href="https://line.me/R/ti/g/nXrUsbdwp6">https://line.me/R/ti/g/nXrUsbdwp6</a></div>
             </div>
             <div class="col-lg-6 mb-4">
-                <img class="img-thumbnail"  src="images/tcas/Line68-Sat-Sun.jpg" style="max-width:300px; width:100%;">
-                <div><strong>CS-VRU#68 (เสาร์-อาทิตย์)</strong></div>
-                <div><a href="https://line.me/R/ti/g/SciDXAHVfJ">https://line.me/R/ti/g/SciDXAHVfJ</a></div>
+                <img class="img-thumbnail"  src="images/tcas/Line69-Sat-Sun.jpg" style="max-width:300px; width:100%;">
+                <div><strong>CS-VRU#69 (เสาร์-อาทิตย์)</strong></div>
+                <div><a href="https://line.me/R/ti/g/8TTT4FLvnH">https://line.me/R/ti/g/8TTT4FLvnH</a></div>
             </div>
         </div>
     </div>
